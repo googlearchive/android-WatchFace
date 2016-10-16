01a9537435603b81d4ed46768b6540b3a83ccbd2
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2016-10-17 00:58:28 +0530

    Add missing import

--- Wearable/src/main/java/com/example/android/wearable/watchface/DigitalWatchFaceService.java
+++ Wearable/src/main/java/com/example/android/wearable/watchface/DigitalWatchFaceService.java
@@ -28,6 +28,7 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.content.ContextCompat;
 import android.support.wearable.watchface.CanvasWatchFaceService;
 import android.support.wearable.watchface.WatchFaceService;
 import android.support.wearable.watchface.WatchFaceStyle;
